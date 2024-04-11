0eeccdfb597e7b66b91846413f0d5cc8f23043b8
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-04-11 10:14:40 -0700

    Update `response_2d.py`
    So that results are stored in the database instead of individual
    files.

--- src/structural_analysis/response_2d.py
+++ src/structural_analysis/response_2d.py
@@ -2,9 +2,11 @@
 Run nonlinear time-history analysis to get the building's response
 """
 
+from pathlib import Path
 import importlib
 import argparse
 from typing import Union
+import pickle
 import numpy as np
 import pandas as pd
 from osmg import solver
@@ -12,12 +14,35 @@ from osmg.gen.query import ElmQuery
 from osmg.ground_motion_utils import import_PEER
 from src.util import store_info
 from extra.structural_analysis.src.util import retrieve_peer_gm_data
+from extra.structural_analysis.src.db import DB_Handler
 
 
 def main():
     # ~~~~~~~~~~~~~~~~~~~~~~ #
     # set up argument parser #
     # ~~~~~~~~~~~~~~~~~~~~~~ #
+
+    # # debugging
+    # import sys
+    # sys.argv = [
+    #     "python",
+    #     "--archetype",
+    #     "scbf_9_ii",
+    #     "--suite_type",
+    #     "cms",
+    #     "--pulse",
+    #     "--hazard_level",
+    #     "4",
+    #     "--gm_number",
+    #     "2",
+    #     "--analysis_dt",
+    #     "0.01",
+    #     "--direction",
+    #     "y",
+    #     "--progress_bar",
+    #     '--damping',
+    #     "modal",
+    # ]
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--archetype")
@@ -27,9 +52,7 @@ def main():
     parser.add_argument("--gm_number")
     parser.add_argument("--analysis_dt")
     parser.add_argument("--direction")
-    parser.add_argument("--output_dir_name", default='individual_files')
     parser.add_argument("--progress_bar", default=False, action='store_true')
-    parser.add_argument('--custom_path', default=None)
     parser.add_argument('--damping', default='modal')
 
     args = parser.parse_args()
@@ -40,23 +63,8 @@ def main():
     gm_number = int(args.gm_number)
     analysis_dt = float(args.analysis_dt)
     direction = args.direction
-    output_dir_name = args.output_dir_name
     progress_bar = bool(args.progress_bar)
-    custom_path = args.custom_path
     damping = args.damping
-
-    # # debugging
-    # archetype = 'scbf_9_ii'
-    # suite_type = 'cms'
-    # pulse = False
-    # hazard_level = '4'
-    # gm_number = 2
-    # analysis_dt = 0.01
-    # direction = 'y'
-    # output_dir_name = 'individual_files'
-    # progress_bar = True
-    # custom_path = '/tmp/test'
-    # damping = 'modal'
 
     def split_archetype(archetype):
         system, stories, rc = archetype.split('_')
@@ -73,9 +81,6 @@ def main():
         raise ValueError(f"Invalid archetype code: {archetype}") from exc
 
     mdl, loadcase = archetype_builder(direction)
-
-    # from osmg.graphics.preprocessing_3d import show
-    # show(mdl, loadcase, extrude=True)
 
     num_levels = len(mdl.levels) - 1
     level_heights = np.diff([level.elevation for level in mdl.levels.values()])
@@ -105,9 +110,7 @@ def main():
         rsn = int(
             df_records.at[(archetype, f"hz_{hazard_level}", "RSN"), str(gm_number)]
         )
-        scaling = df_records.at[
-            (archetype, f"hz_{hazard_level}", "SF"), str(gm_number)
-        ]
+        scaling = df_records.at[(archetype, f"hz_{hazard_level}", "SF"), str(gm_number)]
 
         dir_idx = {"x": 0, "y": 1}
         try:
@@ -120,10 +123,11 @@ def main():
 
     elif suite_type == 'cms':
         df_records = pd.read_csv(
-            "extra/structural_analysis/results/site_hazard/"
-            "ground_motions_cms.csv",
+            "extra/structural_analysis/results/site_hazard/" "ground_motions_cms.csv",
             index_col=[0, 1, 2, 3, 4],
         )
+        df_records.sort_index(axis=0, inplace=True)
+        df_records.sort_index(axis=1, inplace=True)
 
         rsn = df_records.loc[
             (*split_archetype(archetype), int(hazard_level), pulse), 'rsn'
@@ -143,18 +147,6 @@ def main():
 
     else:
         raise NotImplementedError(f'Unsupported suite type: {suite_type}')
-
-    if custom_path:
-        output_folder = custom_path
-    else:
-        output_folder = (
-            f"extra/structural_analysis/results/{archetype}/"
-            f"{output_dir_name}/{hazard_level}/gm{gm_number}"
-        )
-
-    # from osmg.graphics.preprocessing_3d import show
-    # show(mdl, loadcase, extrude=True)
-    # show(mdl, loadcase, extrude=False)
 
     #
     # modal analysis
@@ -202,11 +194,11 @@ def main():
     else:
         raise ValueError(f"Invalid damping type: {damping}")
 
+    record_id = '::'.join([str(x) for x in vars(args).values()])
+    log_file = f'extra/structural_analysis/results/logs/{record_id}'
+
     nlth = solver.THAnalysis(mdl, {loadcase.name: loadcase})
-    nlth.output_directory = output_folder
-    nlth.settings.log_file = store_info(
-        f"{output_folder}/log_{direction}", [gm_filename]
-    )
+    nlth.settings.log_file = log_file
     nlth.settings.restrict_dof = [False, True, False, True, False, True]
     nlth.settings.store_fiber = False
     nlth.settings.store_forces = False
@@ -227,6 +219,13 @@ def main():
         dampen_out_residual=True,
         finish_time=0.00,  # means run the entire file
     )
+
+    # get log contents
+    with open(log_file, 'r') as file:
+        log_contents = file.read()
+    # get session metadata
+
+    info = store_info(input_data_paths=[gm_filename])
 
     # store response quantities
 
@@ -266,14 +265,29 @@ def main():
 
     df[f"Vb-0-{j}"] = nlth.retrieve_base_shear(loadcase.name)[:, 0]
 
-    df.columns = pd.MultiIndex.from_tuples(
-        [x.split("-") for x in df.columns.to_list()]
-    )
+    df.columns = pd.MultiIndex.from_tuples([x.split("-") for x in df.columns.to_list()])
     df.sort_index(axis=1, inplace=True)
 
-    df.to_parquet(
-        store_info(f"{output_folder}/results_{direction}.parquet", [gm_filename])
+    # add the results to the database
+
+    db_handler = DB_Handler(
+        db_path=Path('extra/structural_analysis/results/time_history.db')
     )
+    try:
+        db_handler.store_data(
+            identifier=record_id, dataframe=df, metadata=info, log_content=log_contents
+        )
+    except:
+        # in case it fails, pickle the result variables and save them
+        # with a unique name
+        out = {
+            'identifier': record_id,
+            'dataframe': df,
+            'metadata': info,
+            'log_content': log_contents,
+        }
+        with open(Path(f'extra/structural_analysis/results/{record_id}'), 'wb') as f:
+            pickle.dump(out, f)
 
 
 if __name__ == '__main__':
