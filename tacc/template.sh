7d777919ab9d30637318c0c64017307a5d7f21ec
usr83847 <usr83847@login4.stampede3.tacc.utexas.edu>
2024-05-02 14:01:58 -0500

    Update `template.sh` for Stampede3

--- tacc/template.sh
+++ tacc/template.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #SBATCH -J %jobname%
-#SBATCH -o extra/structural_analysis/tacc/%jobname%_log_%j
+#SBATCH -o extra/structural_analysis/tacc/%jobname%_log.o%j
+#SBATCH -e extra/structural_analysis/tacc/%jobname%_log.e%j
+#SBATCH -p skx                          # Queue (partition) name
 #SBATCH -N %num_nodes%                  # number of nodes requested
 #SBATCH -n %num_tasks%                  # total number of tasks to run in parallel
-#SBATCH -p %partition%                  # queue (partition)
 #SBATCH -t %time%                       # run time (hh:mm:ss)
 #SBATCH -A DesignSafe-HPC4PBEE          # Allocation name to charge job against
 #SBATCH --mail-type=all
 #SBATCH --mail-user=ioannis_vm@berkeley.edu
 
-source $HOME/env_setup.sh
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-export LAUNCHER_WORKDIR=/scratch1/07506/usr83847/2023-11_RID_Realizations/
+source $HOME/.bashrc
+micromamba activate rid_prj
+
 export OMP_NUM_THREADS=1
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+export LAUNCHER_WORKDIR=/scratch/07506/usr83847/2023-11_RID_Realizations/
 export LAUNCHER_JOB_FILE=extra/structural_analysis/tacc/%jobname%_taskfile
-${LAUNCHER_DIR}/paramrun
+
+ibrun pylaunchermpi
