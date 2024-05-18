0625008bf9ba1674913e6c152aa76d7b795dc644
Ioannis Vouvakis Manousakis <usr83847@c455-001.stampede3.tacc.utexas.edu>
2024-05-18 04:38:17 -0500

    Update `obtain_edps.py` to use more cores.

--- src/structural_analysis/obtain_edps.py
+++ src/structural_analysis/obtain_edps.py
@@ -3,9 +3,10 @@ Read time-history analysis results form databases and extract the
 relevant EDPs
 """
 
+import os
+import sys
 import pickle
 import pandas as pd
-from tqdm import tqdm
 from extra.structural_analysis.src.db import DB_Handler
 
 
@@ -42,7 +43,7 @@ def obtain_edps(dataframe):
         Dataframe containing the EDPs
 
     """
-    edps = dataframe.abs().max().drop(['Rtime', 'Subdiv', 'Vb', 'time'])
+    edps = dataframe.abs().max().drop(['Rtime', 'Subdiv', 'Vb'])
     edps['FA'] /= 386.22
     edps.index = pd.MultiIndex.from_tuples(
         [(f'P{x[0]}', x[1], x[2]) for x in edps.index]
@@ -50,26 +51,54 @@ def obtain_edps(dataframe):
     rid = dataframe.iloc[-1, :]['ID'].abs()
     rid.index = pd.MultiIndex.from_tuples([('RID', x[0], x[1]) for x in rid.index])
     edps = pd.concat((edps, rid), axis=0)
+    return edps
 
 
 def main():
-    database_paths = {
-        'extra/structural_analysis/results/results.sqlite',
-    }
 
+    i = int(sys.argv[1])
+    # i ranges from 0 to 4 (5 values)
+
+    issue_dict_path = 'extra/structural_analysis/results/edps_issue.pickle'
+    if not os.path.isfile(issue_dict_path):
+        issue = []
+        with open(issue_dict_path, 'wb') as f:
+            pickle.dump(issue, f)
+    else:
+        with open(issue_dict_path, 'rb') as f:
+            issue = pickle.load(f)
+
+    database_paths = [
+        'extra/structural_analysis/results/results_01.sqlite',
+        'extra/structural_analysis/results/results_02.sqlite',
+        'extra/structural_analysis/results/results_03.sqlite',
+        'extra/structural_analysis/results/results_04.sqlite',
+        'extra/structural_analysis/results/results_05.sqlite',
+        'extra/structural_analysis/results/results_1.sqlite',
+        'extra/structural_analysis/results/results_2.sqlite',
+        'extra/structural_analysis/results/results_3.sqlite',
+        'extra/structural_analysis/results/results_4.sqlite',
+        'extra/structural_analysis/results/results_5.sqlite',
+        'extra/structural_analysis/results/results_6.sqlite',
+        'extra/structural_analysis/results/results_7.sqlite',
+        'extra/structural_analysis/results/results_8.sqlite',
+        'extra/structural_analysis/results/results_9.sqlite',
+        'extra/structural_analysis/results/results_10.sqlite',
+    ][i*3:i*3+3]
+    
     result_db_handler = DB_Handler(
         db_path='extra/structural_analysis/results/edps.sqlite'
     )
     processed_identifiers = set(result_db_handler.list_identifiers())
 
-    issue = []
     already_processed = []
 
-    for path in database_paths:
+    for i, path in enumerate(database_paths):
+        print(f'Processing path {i + 1} out of {len(database_paths)}.', flush=True)
         db_handler = DB_Handler(db_path=path)
         identifiers = db_handler.list_identifiers()
 
-        for identifier in tqdm(identifiers):
+        for j, identifier in enumerate(identifiers):
             if identifier in processed_identifiers:
                 already_processed.append(identifier)
                 continue
@@ -81,7 +110,8 @@ def main():
             else:
                 issue.append((status, identifier))
                 db_handler.delete_record(identifier)
-    with open('extra/structural_analysis/results/edps_issue.pickle', 'wb') as f:
+
+    with open(issue_dict_path, 'wb') as f:
         pickle.dump(issue, f)
 
 
